fe62e196f493da226a5b462f6fa4bf61cb6d7817
Teng <1766707503@qq.com>
2022-04-13 22:18:11 +0800

    添加《算法》第四版习题答案

---
+++ fourth/section1/Exercise8.java
@@ -0,0 +1,10 @@
+package section1;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        System.out.println('b');
+        System.out.println('b' + 'c');
+        System.out.println((char) ('a' + 4));
+        System.out.println('a' + 4);
+    }
+}
